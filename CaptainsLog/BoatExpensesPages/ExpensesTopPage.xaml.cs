2d5c49df4c3425aa8c50a43c333a7219c9709a08
DevKaneB <devkanebrennan@icloud.com>
2025-12-28 07:08:09 +0000

    Expense items buttons for averages shown

--- CaptainsLog/BoatExpensesPages/ExpensesTopPage.xaml.cs
+++ CaptainsLog/BoatExpensesPages/ExpensesTopPage.xaml.cs
@@ -1,27 +1,64 @@
 using CaptainsLog.BoatProfilePages;
+using CaptainsLog.DatabaseClasses;
+using System.Diagnostics;
 
 namespace CaptainsLog.BoatExpensesPages;
 
 public partial class ExpensesTopPage : ContentPage
 {
+    ExpensesSQLTools SQLTools;
+
 	public ExpensesTopPage()
 	{
 		InitializeComponent();
-	}
+        SQLTools = new ExpensesSQLTools();
+    }
 
     async void OnWeeklyExpensesClicked(object? sender, EventArgs e)
     {
-        
+        try
+        {
+            var AmountItem = await SQLTools.GetItemsViaQueryAsync("SELECT printf('%.2f',SUM(Amount) / 3) * 1 AS AMOUNT From ExpensesItem Where ExpenseDate > DATE('now','-21 days')");
+            AvgExpensesLbl.Text = AmountItem[0].Amount.ToString("F2") + "p/w";
+            AmountItem.Clear();
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
     }
 
     async void OnMonthlyExpensesClicked(object? sender, EventArgs e)
     {
+        try
+        {
+            var AmountItem = await SQLTools.GetItemsViaQueryAsync("SELECT printf('%.2f',SUM(Amount) / 3) * 1 AS AMOUNT From ExpensesItem Where ExpenseDate > DATE('now','-90 days')");
+            AvgExpensesLbl.Text = AmountItem[0].Amount.ToString("F2") + "p/m";
+            AmountItem.Clear();
 
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+
+        }
     }
 
     async void OnYearlyExpensesClicked(object? sender, EventArgs e)
     {
+        try
+        {
+            var AmountItem = await SQLTools.GetItemsViaQueryAsync("SELECT printf('%.2f',SUM(Amount) / 3) * 1 AS AMOUNT From ExpensesItem Where ExpenseDate > DATE('now','-365 days')");
+            AvgExpensesLbl.Text = AmountItem[0].Amount.ToString("F2") + "p/y";
+            AmountItem.Clear();
 
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+
+        }
     }
 
     async void OnAddExpenseClicked(object? sender, EventArgs e)
